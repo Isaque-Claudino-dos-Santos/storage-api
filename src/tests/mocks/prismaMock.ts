4269cc792d182b9b7e60723ae9d1d04053d62fef
isaque.claudino <isaqueclaudino12@gmail.com>
2024-11-19 12:02:03 -0300

    mock prisma

--- src/tests/mocks/prismaMock.ts
+++ src/tests/mocks/prismaMock.ts
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client'
 import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended'
 
 import prisma from '../../prisma'
 
 jest.mock('../../prisma', () => ({
     __esModule: true,
-    default: mockDeep<PrismaClient>(),
+    default: mockDeep<DeepMockProxy<typeof prisma>>({ funcPropSupport: true }),
 }))
 
 beforeEach(() => {
     mockReset(prismaMock)
 })
 
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
+export const prismaMock = prisma as unknown as DeepMockProxy<typeof prisma>
